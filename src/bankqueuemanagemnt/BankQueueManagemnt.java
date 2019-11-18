b5b04ef309dca007658ff3431680cfbfe52791ee
Asim Zahid <asimzahid02@gmail.com>
2019-11-18 12:29:58 +0500

    OS project, Bank queue management, multi-threading, socket programming, scheduling algorithms

---
+++ src/bankqueuemanagemnt/BankQueueManagemnt.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankqueuemanagemnt;
+
+
+
+/**
+ *
+ * @author asimz
+ */
+public class BankQueueManagemnt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SplashScreen obj = new SplashScreen();
+        obj.setVisible(true);
+        
+    }
+    
+}
